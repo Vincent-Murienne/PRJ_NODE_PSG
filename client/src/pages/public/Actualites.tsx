a498fe45309df3343ab0928c80733c75c70c618b
hadilouakkouche <hadil@shift89.fr>
2024-11-15 10:59:41 +0100

    Actualite + Contact + ML

--- client/src/pages/public/Actualites.tsx
+++ client/src/pages/public/Actualites.tsx
@@ -1,9 +1,178 @@
-import { Component } from 'react'
+// import React from 'react';
+// import  { useEffect, useState } from 'react';
 
-export default class Actualites extends Component {
-  render() {
-    return (
-      <div>Actualites</div>
-    )
-  }
+// //import Slider from 'react-slick';
+// //import 'slick-carousel/slick/slick.css';
+// //import 'slick-carousel/slick/slick-theme.css';
+// //import '../../assets/css/Actualites/Actualites.css';
+// import axios from 'axios';
+
+// const Actualites = () => {
+//   // const settings = {
+//   //   dots: true,
+//   //   infinite: true,
+//   //   speed: 500,
+//   //   slidesToShow: 2.5,
+//   //   slidesToScroll: 2,
+//   //   responsive: [
+//   //     {
+//   //       breakpoint: 768,
+//   //       settings: {
+//   //         slidesToShow: 2,
+//   //       },
+//   //     },
+//   //   ],
+//   // };
+
+// interface Actualite {
+//   id_actualite: number;
+//   titre: string;
+//   date: string;
+//   texte_long: string;
+//   resume: string;
+//   image: string;
+// }
+
+// //   return (
+// //     <div className="actualites-container">
+// //       <h1 className="actualites-title">Actualités</h1>
+// //       <Slider {...settings}>
+// //         <div className="actualites-item">
+// //           <h3 className="actualites-subTitle">Actualité une</h3>
+// //           <p className="actualites-paragraph">Celle-ci est une première actualité</p>
+// //         </div>
+// //         <div className="actualites-item">
+// //           <h3 className="actualites-subTitle">Actualité deux</h3>
+// //           <p className="actualites-paragraph">Celle-ci est une deuxième actualité</p>
+// //         </div>
+// //         <div className="actualites-item">
+// //           <h3 className="actualites-subTitle">Actualité trois</h3>
+// //           <p className="actualites-paragraph">Celle-ci est une troisième actualité</p>
+// //         </div>
+// //         <div className="actualites-item">
+// //           <h3 className="actualites-subTitle">Actualité quatre</h3>
+// //           <p className="actualites-paragraph">Celle-ci est une quatrième actualité</p>
+// //         </div>
+// //         <div className="actualites-item">
+// //           <h3 className="actualites-subTitle">Actualité cinq</h3>
+// //           <p className="actualites-paragraph">Celle-ci est une cinquième actualité</p>
+// //         </div>
+// //       </Slider>
+// //     </div>
+// //   );
+// // };
+
+// // export default Actualites;
+// //const Actualites: React.FC = () => {
+//   const [actualites, setActualites] = useState<Actualite[]>([]);
+//   const [loading, setLoading] = useState<boolean>(true);
+
+//   useEffect(() => {
+//     const fetchActualites = async () => {
+//       try {
+//         const response = await axios.get('/api/actualites'); 
+//         console.log(response.data);
+//         setActualites(response.data);
+//       } catch (error) {
+//         console.error("Erreur lors de la récupération des actualités :", error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchActualites();
+//   }, []);
+
+//   if (loading) {
+//     return <div>Chargement des actualités...</div>;
+//   }
+//   fetch(`http://localhost:8080/api/actualites`)
+//       .then(res => res.json())
+//       .then(data => setActualites(data))
+//       .catch(error => console.error('Erreur lors de la récupération des actualités:', error));
+
+//   return (
+//     <div className="actualites-container">
+//       <h1>Actualités</h1>
+//       {actualites.map((actualite) => (
+//         <div key={actualite.id_actualite} className="actualite-item">
+//           <h2>{actualite.titre}</h2>
+//           <p>{actualite.resume}</p>
+//           {actualite.image && <img src={actualite.image} alt={actualite.titre} />}
+//           <p>{actualite.texte_long}</p>
+//           <small>{new Date(actualite.date).toLocaleDateString()}</small>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+// //}
+
+// export default Actualites;
+import React, { useEffect, useState } from 'react';
+import "../../assets/css/Actualites/Actualites.css"
+
+// Définition de l'interface pour les données d'actualité
+interface Actualite {
+  id_actualite: number;
+  titre: string;
+  date: string;
+  texte_long: string;
+  resume: string;
+  image: string;
 }
+
+const Actualites: React.FC = () => {
+  const [actualites, setActualites] = useState<Actualite[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchActualites = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/api/actualites');
+        if (!response.ok) {
+          throw new Error(`Erreur HTTP : ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data); // Vérifiez si c'est bien un tableau d'actualités
+
+        setActualites(Array.isArray(data) ? data : []);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des actualités :", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchActualites();
+  }, []);
+
+  if (loading) {
+    return <div>Chargement des actualités...</div>;
+  }
+
+  return (
+   
+  
+    <div>
+      <div className="actualites-title">Toutes les Actualités</div>
+      <div className="actualites-container">
+      {actualites.length > 0 ? (
+        actualites.map((actualite) => (
+          <div key={actualite.id_actualite} className="actualite-item">
+            <h2 className="actualites-subTitle">{actualite.titre}</h2>
+            <small>{new Date(actualite.date).toLocaleDateString()}</small>
+            <p className="actualites-paragraph">{actualite.resume}</p>
+            {actualite.image && <img src={actualite.image} alt={actualite.titre} />}
+            <p>{actualite.texte_long}</p>
+          </div>
+        ))
+      ) : (
+        <p>Aucune actualité disponible.</p>
+      )}
+    </div>
+    </div>
+  );
+};
+
+export default Actualites;
--- client/src/pages/public/Contact.tsx
+++ client/src/pages/public/Contact.tsx
@@ -1,149 +1,65 @@
-import React, { useState,CSSProperties } from 'react';
+import React, { useState } from 'react';
+import '../../assets/css/Contact/contact.css';
 
-const ContactForm = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-
-  const [errors, setErrors] = useState({
+const ContactPage = () => {
+  const [formData, setFormData] = useState({
     name: '',
     email: '',
     message: '',
   });
 
-  const validate = () => {
-    let valid = true;
-    const errors = {
-      name: '',
-      email: '',
-      message: '',
-    };
-
-    if (!name) {
-      errors.name = 'Le nom est requis';
-      valid = false;
-    }
-
-    if (!email) {
-      errors.email = 'L\'email est requis';
-      valid = false;
-    } else if (!/\S+@\S+\.\S+/.test(email)) {
-      errors.email = 'L\'email n\'est pas valide';
-      valid = false;
-    }
-
-    if (!message) {
-      errors.message = 'Le message est requis';
-      valid = false;
-    }
-
-    setErrors(errors);  // Mise à jour de l'état des erreurs
-    return valid;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
   };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (validate()) {
-      alert('Formulaire soumis');
-    }
+    
+    console.log('Form Submitted:', formData);
   };
-  
 
   return (
-    <div style={styles.container}>
-      <h1 style={styles.title}>Contactez-nous</h1>
-      <form onSubmit={handleSubmit} style={styles.form}>
-        <div style={styles.formGroup}>
-          <label style={styles.label}>Nom :</label>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            style={styles.input}
-          />
-          {errors.name && <span style={styles.error}>{errors.name}</span>} {/* Affichage de l'erreur */}
-        </div>
-
-        <div style={styles.formGroup}>
-          <label style={styles.label}>Email :</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            style={styles.input}
-          />
-          {errors.email && <span style={styles.error}>{errors.email}</span>} {/* Affichage de l'erreur */}
-        </div>
-
-        <div style={styles.formGroup}>
-          <label style={styles.label}>Message :</label>
-          <textarea
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            style={styles.textarea}
-          />
-          {errors.message && <span style={styles.error}>{errors.message}</span>} {/* Affichage de l'erreur */}
-        </div>
-
-        <button type="submit" style={styles.button}>Envoyer</button>
+    <div className="contact-container">
+      <h1 className="contact-title">Contactez-nous</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          className="contact-input"
+          placeholder="Nom"
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          className="contact-input"
+          placeholder="Email"
+          required
+        />
+        <textarea
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          className="contact-textarea"
+          placeholder="Votre message"
+          required
+        />
+        <button type="submit" className="contact-button">
+          Envoyer
+        </button>
       </form>
+      <div className="contact-footer">
+        <p>Vous pouvez aussi nous contacter via <a href="mailto:support@contact.com">support@contact.com</a></p>
+      </div>
     </div>
   );
 };
 
-const styles: { [key: string]: CSSProperties } = {
-  container: {
-    maxWidth: '600px',
-    margin: '0 auto',
-    padding: '20px',
-    backgroundColor: '#f9f9f9',
-    borderRadius: '8px',
-    boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
-  },
-  title: {
-    textAlign: 'center',
-    marginBottom: '20px',
-  },
-  form: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  formGroup: {
-    marginBottom: '15px',
-  },
-  label: {
-    marginBottom: '5px',
-    fontWeight: 'bold',
-  },
-  input: {
-    padding: '10px',
-    fontSize: '16px',
-    border: '1px solid #ccc',
-    borderRadius: '4px',
-    width: '100%',
-  },
-  textarea: {
-    padding: '10px',
-    fontSize: '16px',
-    border: '1px solid #ccc',
-    borderRadius: '4px',
-    width: '100%',
-    height: '100px',
-  },
-  button: {
-    padding: '10px 15px',
-    fontSize: '16px',
-    backgroundColor: '#007BFF',
-    color: '#fff',
-    border: 'none',
-    borderRadius: '4px',
-    cursor: 'pointer',
-  },
-  error: {
-    color: 'red',
-    fontSize: '12px',
-    marginTop: '5px',
-  },
-};
-
-export default ContactForm;
+export default ContactPage;
